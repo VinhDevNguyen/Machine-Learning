35d99fa9128f0fbedba2561481cb096b09fdadc0
VinhDevNguyen <vinh2000ld14@gmail.com>
2020-03-27 14:03:25 +0700

    Update use summary of Exercise 5

--- Week1/Vinh_Exercise/Exercise5.py
+++ Week1/Vinh_Exercise/Exercise5.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+import statsmodels.formula.api as smf
 
 
 # %% [markdown]
@@ -17,13 +18,13 @@ df.head()
 
 # %%
 # ###  3. Plot data
-plt.scatter(df[['X1']],df[['y']])
+plt.scatter(df[['X1']],df[['y']],color='blue')
 plt.xlabel('X1')
 plt.ylabel('y')
 
 
 # %%
-plt.scatter(df[['X2']],df[['y']])
+plt.scatter(df[['X2']],df[['y']], color = 'red')
 plt.xlabel('X2')
 plt.ylabel('y')
 
@@ -42,26 +43,33 @@ print('Intercept: ', Regress.intercept_)
 print('Coeficients: ', Regress.coef_)
 
 # %% [markdown]
-# ### 6. Ta bỏ bớt X1 để xem sự thay đổi của $\hat{\theta}$
-Regress.fit(df[['X2']], y_train)
-print('Intercept: ', Regress.intercept_)
-print('Coeficients: ', Regress.coef_)
+# ### 6. Ta bỏ bớt X1 để xem sự thay đổi của nó
+result = smf.ols('y ~ X2', df).fit()
+result.summary()
 
+# %% [markdown]
+# ### 7. Ta bỏ bớt thử X2 để xem sự thay đổi của nó
+result = smf.ols('y ~ X1', df).fit()
+result.summary()
+# * Ta thấy khi bỏ bớt X1 và X2 đi thì ta thấy hệ số $R^2$ bị giảm
 # %% [markdown]
 # * Ta thấy rằng khi bỏ bớt X1 thì thì $\hat{\theta}$ thay đổi
 # ### 7. Thêm vào cột $X^{2}_2$ để xem sự thay đổi của $\hat{\theta}$
-X22 = np.full((10,1),8.0)
+X22 = df['X2'].pow(2)
 df['X22'] = X22
 df.head()
 
-# %% 
-x_train = df[['X1', 'X2', 'X22']]
-y_train = df[['y']]
-Regress = LinearRegression()
-Regress.fit(x_train,y_train)
-print('Intercept: ', Regress.intercept_)
-print('Coeficients: ', Regress.coef_)
+X23 = df['X2'].pow(3)
+df['X23'] = X23
+df.head()
+result23 = smf.ols('y ~ X1 + X2 + X22 + X23', data= df).fit()
+result22 = smf.ols('y ~ X1 + X2 + X22', data = df).fit()
+# %%
+result22.summary()
+
+# %%
+result23.summary()
 
 # %% [markdown]
 # ### 8. Summary
-# * Khi thêm cột X22 vào thì ta thấy phương trình hồi quy không có sự thay đổi
+# * Khi thêm cột X2_2 vào thì ta thấy hệ số $R^2$ đều tăng rất mạnh
